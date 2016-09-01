62a7ac4405964b35fbc8c9c17c9f6404fc123bb2
evalphobia <evalphobia@gmail.com>
2016-09-01 13:29:13 +0900

    Update fluent hook to use filter

--- log/fluent/fluent.go
+++ log/fluent/fluent.go
@@ -1,6 +1,8 @@
 package fluent
 
 import (
+	"net/http"
+
 	"github.com/Sirupsen/logrus"
 	"github.com/evalphobia/logrus_fluent"
 )
@@ -22,8 +24,38 @@ func AddLevel(level logrus.Level) {
 	hookLevel = append(hookLevel, level)
 }
 
-func Set(host string, port int) {
-	hook := logrus_fluent.NewHook(host, port)
+func Set(host string, port int) error {
+	hook, err := logrus_fluent.New(host, port)
+	if err != nil {
+		return err
+	}
+
 	hook.SetLevels(hookLevel)
+	hook.AddIgnore("context")
+	hook.AddFilter("http_request", filterRequest)
+	hook.AddFilter("error", logrus_fluent.FilterError)
+
 	logrus.AddHook(hook)
+	return nil
+}
+
+func filterRequest(v interface{}) interface{} {
+	req, ok := v.(*http.Request)
+	if !ok {
+		return v
+	}
+
+	return request{
+		Method:     req.Method,
+		Host:       req.Host,
+		RequestURI: req.RequestURI,
+		RemoteAddr: req.RemoteAddr,
+	}
+}
+
+type request struct {
+	Method     string
+	Host       string
+	RequestURI string
+	RemoteAddr string
 }
