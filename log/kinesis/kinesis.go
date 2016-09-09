16fed0ba93fb82291096fa31403274a5bda156b8
evalphobia <evalphobia@gmail.com>
2016-09-09 16:56:37 +0900

    Add kinesis hook

---
+++ log/kinesis/kinesis.go
@@ -0,0 +1,79 @@
+package kinesis
+
+import (
+	"net/http"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/evalphobia/logrus_kinesis"
+
+	"github.com/evalphobia/go-log-wrapper/log"
+)
+
+// default logging hook level
+var hookLevel = []logrus.Level{
+	logrus.PanicLevel,
+	logrus.ErrorLevel,
+}
+
+func SetLevels(levels []logrus.Level) {
+	hookLevel = levels
+}
+
+func AddLevel(level logrus.Level) {
+	hookLevel = append(hookLevel, level)
+}
+
+func Set(name string, conf Config) error {
+	hook, err := logrus_kinesis.New(name, logrus_kinesis.Config{
+		AccessKey: conf.AccessKey,
+		SecretKey: conf.SecretKey,
+		Region:    conf.Region,
+	})
+	if err != nil {
+		return err
+	}
+
+	hook.SetLevels(hookLevel)
+	hook.AddIgnore("context")
+	hook.AddFilter("trace", filterTrace)
+	hook.AddFilter("http_request", filterRequest)
+	hook.Async()
+	logrus.AddHook(hook)
+	return nil
+}
+
+type Config struct {
+	AccessKey string
+	SecretKey string
+	Region    string
+}
+
+func filterTrace(v interface{}) interface{} {
+	trace, ok := v.([]log.StackTrace)
+	if !ok || len(trace) == 0 {
+		return v
+	}
+
+	return trace[0]
+}
+
+func filterRequest(v interface{}) interface{} {
+	req, ok := v.(*http.Request)
+	if !ok {
+		return v
+	}
+
+	return request{
+		Method:     req.Method,
+		Host:       req.Host,
+		RequestURI: req.RequestURI,
+		RemoteAddr: req.RemoteAddr,
+	}
+}
+
+type request struct {
+	Method     string
+	Host       string
+	RequestURI string
+	RemoteAddr string
+}
